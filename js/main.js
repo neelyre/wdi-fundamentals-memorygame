e6cc7d52c4195130e15de0cdf7fd286ac14e01df
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-06-22 19:44:52 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+   
+   if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a Match!");
+}
+else {
+	alert("Sorry, try again!");
+}
+}
+console.log("User flipped " + cardsInPlay);
+
