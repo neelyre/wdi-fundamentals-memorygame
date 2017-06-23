3f949c3cf7b46cc0b9d55f1d6a27f45496218699
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-06-23 09:54:14 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
    if (cardsInPlay[0] === cardsInPlay[1]){
@@ -10,9 +31,11 @@ else {
 }
 }
 var flipcard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 flipcard(0)
