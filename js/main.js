fabe63d88aca19e0aa27f552840c8a0d0da65bfb
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-06-25 09:59:23 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -23,23 +23,45 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function(){
-   if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a Match!");
-}
-else {
-	alert("Sorry, try again!");
-}
-}
-var flipcard = function(cardId){
+
+
+
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    }   else {
+        alert("Sorry, try again.");
+    }
+};
+
+var flipcard = function(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+
+
+   if (cardsInPlay.length === 2) {
+    
+    checkForMatch();
+    }
+};
+
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipcard);
+        document.getElementById('game-board').appendChild(cardElement);
+        
+	}
 }
-flipcard(0)
-flipcard(1)
+	createBoard ();
+
+
 
 
 
